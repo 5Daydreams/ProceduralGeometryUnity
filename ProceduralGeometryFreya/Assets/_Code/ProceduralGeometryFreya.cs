c415ed3122af5eeabac5cffed76b7ea0a73bd524
5Daydreams <49330163+5Daydreams@users.noreply.github.com>
2021-08-07 23:36:13 -0300

    + Adding uv's and normals;

--- ProceduralGeometryFreya/Assets/_Code/ProceduralGeometryFreya.cs
+++ ProceduralGeometryFreya/Assets/_Code/ProceduralGeometryFreya.cs
@@ -1,33 +1,51 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class ProceduralGeometryFreya : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     private MeshFilter _meshFilter;
 
-    private void Awake() 
+    private void OnEnable()
     {
         Mesh mesh = new Mesh();
         mesh.name = "Procedural Quad";
 
         List<Vector3> points = new List<Vector3>()
         {
-            new Vector3(-1,1),
-            new Vector3(1,1),
-            new Vector3(-1,-1),
-            new Vector3(1,-1)
+            new Vector3 (-1, 1),
+            new Vector3 (1, 1),
+            new Vector3 (-1, -1),
+            new Vector3 (1, -1)
         };
 
-        int[] triIndices = new int[]{
-            1,0,2,
-            3,1,2
+        int[] triIndices = new int[]
+        {
+            2, 0, 1,
+            2, 1, 3
+        };
+
+        List<Vector2> uvs = new List<Vector2>()
+        {
+            new Vector2(0,1),
+            new Vector2(1,1),
+            new Vector2(0,0),
+            new Vector2(1,0)
+        };
+
+        List<Vector3> normals = new List<Vector3>()
+        {
+            new Vector3 (0, 0, 1),
+            new Vector3 (0, 0, 1),
+            new Vector3 (0, 0, 1),
+            new Vector3 (0, 0, 1)
         };
 
         mesh.SetVertices(points);
         mesh.triangles = triIndices;
-
-        mesh.RecalculateNormals();
+        mesh.SetNormals(normals);
+        mesh.SetUVs(0, uvs);
 
         _meshFilter.sharedMesh = mesh;
     }
