f7d7dc9838c1dfc5630144f757f6515882f20889
5Daydreams <49330163+5Daydreams@users.noreply.github.com>
2021-08-07 22:05:35 -0300

    + Adjusting issue with VSCode;

---
+++ ProceduralGeometryFreya/Assets/_Code/ProceduralGeometryFreya.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeometryFreya : MonoBehaviour
+{
+    [SerializeField] 
+    private MeshFilter _meshFilter;
+
+    private void Awake() 
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "Procedural Quad";
+
+        List<Vector3> points = new List<Vector3>()
+        {
+            new Vector3(-1,1),
+            new Vector3(1,1),
+            new Vector3(-1,-1),
+            new Vector3(1,-1)
+        };
+
+        int[] triIndices = new int[]{
+            2,0,1,
+            2,1,3
+        };
+
+        mesh.SetVertices(points);
+        mesh.triangles = triIndices;
+
+        _meshFilter.sharedMesh = mesh;
+    }
+}
