ffd7e6e01d7f8c298d6aaca4dcefc4d7734ceb42
Kiyoshi <nelsonkossuga@gmail.com>
2021-08-09 20:42:02 -0300

    + Added ring-mesh script, but still working on gizmo-level;

--- ProceduralGeometryFreya/Assets/_Code/QuadRing.cs
+++ ProceduralGeometryFreya/Assets/_Code/QuadRing.cs
@@ -1,7 +1,26 @@
-﻿namespace _Code
+﻿using System;
+using Submodules._Code.Extensions;
+using UnityEngine;
+
+namespace _Code
 {
-    public class QuadRing
+    public class QuadRing : MonoBehaviour
     {
-        
+        [Range(0.01f, 2)] [SerializeField] private float _innerRadius;
+        [Range(0.01f, 2)] [SerializeField] private float _thickness;
+
+        private float _outerRadius => _innerRadius + _thickness;
+
+        [Range(3, 32)] [SerializeField] private int _detailLevel;
+
+
+        private void OnDrawGizmosSelected()
+        {
+            // Gizmos.DrawSphere(transform.position, _innerRadius);
+            // Gizmos.DrawSphere(transform.position, _outerRadius);
+
+            GizmoExtensions.DrawWireCircle(transform.position, transform.rotation, _innerRadius, _detailLevel);
+            GizmoExtensions.DrawWireCircle(transform.position, transform.rotation, _outerRadius, _detailLevel);
+        }
     }
 }
