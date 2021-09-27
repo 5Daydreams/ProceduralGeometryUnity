524f76c9c354e941c4bd82cb8e35a5ce057e50e5
5Daydreams <nelsonkossuga@gmail.com>
2021-09-27 20:19:52 +0200

    + Added a spiral coil to the prediction line;

--- ProceduralGeometryFreya/Assets/_Code/Gizmos/UniDirectionalSpiralGizmo.cs
+++ ProceduralGeometryFreya/Assets/_Code/Gizmos/UniDirectionalSpiralGizmo.cs
@@ -12,29 +12,33 @@ public class UniDirectionalSpiralGizmo : MonoBehaviour
     [SerializeField] private int _pointCount;
     [SerializeField] private Color _startColor;
     [SerializeField] private Color _endColor;
+    [SerializeField] private bool _drawAxis;
 
     private void OnDrawGizmos()
     {
-        Vector3 localDirection = (this.transform.rotation * _direction).normalized;
+        Vector3 axisDir = (this.transform.rotation * _direction).normalized;
 
         if (_pointCount <= 2)
             _pointCount = 3;
-        
-        // Gizmos.color = Color.red;
-        // Gizmos.DrawRay(transform.position,localDirection*_coilLength);
 
-        float angleStepSize = 360.0f*_curlFactor / _pointCount;
+        if (_drawAxis)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, axisDir * _coilLength);
+        }
+
+        float angleStepSize = 360.0f * _curlFactor / _pointCount;
         float lengthStepSize = _coilLength / _pointCount;
 
         for (int i = 0; i < _pointCount; i++)
         {
-            Quaternion rotationQuaternion1 = Quaternion.AngleAxis(angleStepSize * i, localDirection);
-            Quaternion rotationQuaternion2 = Quaternion.AngleAxis(angleStepSize * (i + 1), localDirection);
+            Quaternion rotationQuaternion1 = Quaternion.AngleAxis(angleStepSize * i, axisDir);
+            Quaternion rotationQuaternion2 = Quaternion.AngleAxis(angleStepSize * (i + 1), axisDir);
 
-            Vector3 point1 = rotationQuaternion1 * (transform.right * _radius) + (localDirection * (i)* lengthStepSize);
-            Vector3 point2 = rotationQuaternion2 * (transform.right * _radius) + (localDirection * (i+1) *lengthStepSize);
+            Vector3 point1 = rotationQuaternion1 * (transform.right * _radius) + (axisDir * (i) * lengthStepSize);
+            Vector3 point2 = rotationQuaternion2 * (transform.right * _radius) + (axisDir * (i + 1) * lengthStepSize);
 
-            Gizmos.color = Color.Lerp(_startColor,_endColor,i*1.0f/_pointCount);
+            Gizmos.color = Color.Lerp(_startColor, _endColor, i * 1.0f / _pointCount);
             Gizmos.DrawLine(point1, point2);
         }
     }
